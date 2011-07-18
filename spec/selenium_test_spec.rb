fbb0669924ac05038ffedea4c63f2e5151346f67
Eric Pierce <wapcaplet88@gmail.com>
2011-07-18 15:50:56 -0600

    More spec tests

--- spec/selenium_test_spec.rb
+++ spec/selenium_test_spec.rb
@@ -55,5 +55,58 @@ describe Rsel::SeleniumTest do
       end
     end
   end
+
+  context "text fields" do
+    context "type into or fill in field" do
+      it "passes when a text field with the given label exists" do
+        @st.type_into_field("Eric", "First name").should == true
+        @st.fill_in_with("Last name", "Pierce").should == true
+      end
+
+      it "passes when a text field with the given id exists" do
+        @st.type_into_field("Eric", "first_name").should == true
+        @st.fill_in_with("last_name", "Pierce").should == true
+      end
+
+      it "passes when a texarea with the given label exists" do
+        @st.type_into_field("Blah blah blah", "Life story").should == true
+        @st.fill_in_with("Life story", "Jibber jabber").should == true
+      end
+
+      it "passes when a texarea with the given id exists" do
+        @st.type_into_field("Blah blah blah", "biography").should == true
+        @st.fill_in_with("biography", "Jibber jabber").should == true
+      end
+
+      it "fails when no field with the given label or id exists" do
+        @st.type_into_field("Matthew", "Middle name").should == false
+        @st.fill_in_with("middle_name", "Matthew").should == false
+      end
+    end
+
+    context "verify the contents of a text field" do
+      # TODO
+    end
+
+  end
+
+  context "dropdowns" do
+    context "select from a dropdown" do
+      it "passes when a value exists in a dropdown" do
+        @st.select_from_dropdown("Tall", "Height").should == true
+        @st.select_from_dropdown("Medium", "Weight").should == true
+      end
+
+      it "fails when a dropdown exists, but the value doesn't" do
+        @st.select_from_dropdown("Giant", "Height").should == false
+        @st.select_from_dropdown("Obese", "Weight").should == false
+      end
+
+      it "fails when no such dropdown exists" do
+        @st.select_from_dropdown("Over easy", "Eggs").should == false
+      end
+    end
+  end
+
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,24 @@ RSpec.configure do |config|
   config.include Rsel
 end
 
+
+# Monkeypatch the Selenium::Client::Protocol module,
+# to prevent http_post from spewing out a bunch of `puts`es on failure.
+# (It's really distracting when running spec tests)
+module Selenium
+  module Client
+    module Protocol
+      def http_post(data)
+        start = Time.now
+        called_from = caller.detect{|line| line !~ /(selenium-client|vendor|usr\/lib\/ruby|\(eval\))/i}
+        http = Net::HTTP.new(@host, @port)
+        http.open_timeout = default_timeout_in_seconds
+        http.read_timeout = default_timeout_in_seconds
+        response = http.post('/selenium-server/driver/', data, HTTP_HEADERS)
+        # <-- Here is where all the puts statements were -->
+        [ response.body[0..1], response.body ]
+      end
+    end
+  end
+end
+
