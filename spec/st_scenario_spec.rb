eba9179f7b55b110fef369bb6d0d6e615933adda
Eric Pierce <wapcaplet88@gmail.com>
2012-01-15 09:07:24 -0700

    Stub in some scenario specs

---
+++ spec/st_scenario_spec.rb
@@ -0,0 +1,26 @@
+require 'spec/spec_helper'
+
+describe 'scenarios' do
+  before(:each) do
+    @st.visit('/').should be_true
+  end
+
+  describe "#begin_scenario" do
+    it "returns true" do
+      @st.begin_scenario.should be_true
+    end
+
+    it "sets found_failure to false" do
+      @st.found_failure = true
+      @st.begin_scenario
+      @st.found_failure.should be_false
+    end
+  end
+
+  describe "#end_scenario" do
+    it "returns true" do
+      @st.begin_scenario.should be_true
+    end
+  end
+end
+
