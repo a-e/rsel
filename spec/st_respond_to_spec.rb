3fd214616b0a6ac09660129d3f7478fdc688fd5e
Eric Pierce <wapcaplet88@gmail.com>
2014-06-26 15:35:21 -0600

    Use expect instead of should

--- spec/st_respond_to_spec.rb
+++ spec/st_respond_to_spec.rb
@@ -2,15 +2,15 @@ require_relative 'st_spec_helper'
 
 describe 'respond_to?' do
   it "returns true if a method is explicitly defined" do
-    @st.respond_to?('see').should == true
+    expect(@st.respond_to?('see')).to be true
   end
 
   it "returns true if the Selenium::Client::Driver defines the method" do
-    @st.respond_to?('is_element_present').should == true
+    expect(@st.respond_to?('is_element_present')).to be true
   end
 
   it "returns false if the method isn't defined" do
-    @st.respond_to?('junk').should == false
+    expect(@st.respond_to?('junk')).to be false
   end
 end
 
